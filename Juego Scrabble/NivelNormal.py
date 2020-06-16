4ceba76ab7ba86fd8bfbcc4f53665dde5da15ebc
germanEAL <62100321+germanEAL@users.noreply.github.com>
2020-06-16 13:45:31 -0300

    Update NivelNormal.py

--- Juego Scrabble/NivelNormal.py
+++ Juego Scrabble/NivelNormal.py
@@ -1,7 +1,25 @@
-def comprobar_puntaje(lista,Letras):
+def comprobar_puntaje(palabra,coordenadas,Letras,coor_rojos,coor_naranja,coor_azul,coor_celeste):
     puntaje=0
-    for l in lista:
-        puntaje+=Letras[l.lower()][0]
+    valor_letra=0
+    resta_pal=0
+    multip=0
+    for l in range(0,len(palabra)):
+        if coordenadas[l] in coor_azul:
+            valor_letra=Letras[palabra[l].lower()][0]**2
+            puntaje+=valor_letra
+        if coordenadas[l] in coor_celeste:
+            valor_letra=Letras[palabra[l].lower()][0]-1
+            puntaje+=valor_letra
+        if coordenadas[l] in coor_naranja:
+            resta_pal+=2
+        if coordenadas[l] in coor_rojos:
+            multip+=2
+        else:
+            puntaje+=Letras[palabra[l].lower()][0]
+    if multip > 0:
+        puntaje= puntaje**multip
+    puntaje= puntaje - resta_pal
+
     return puntaje
 
 def dar_letras(Letras,cant):
@@ -14,21 +32,12 @@ def dar_letras(Letras,cant):
         lista_letras.append(l[random.randint(0,len(l)-1)])
     return lista_letras
 
-"""def cambiar_letras(window,atril,lista,Letras): REVISION
-    print(atril)
-    atril_v=atril
-    for l in atril:
-        if l in lista:
-            atril.remove(l)
-    print(atril)
-    nuevas_letras=dar_letras(Letras,len(lista))
+def cambiar_letras(window,key_letras,Letras): 
+    nuevas_letras=dar_letras(Letras,len(key_letras)) 
     cant=0
     for l in nuevas_letras:
-        atril.append(l)
-    for l in atril_v:
-        print(l)
-        window.find_element(l).update(text=atril[cant])
-        cant+1"""
+         window.find_element(key_letras[cant]).update(text=l,button_color=('Black','#D8973C'))
+         cant+=1
 
 def leer_top():
     import json
@@ -59,13 +68,20 @@ def main(nombre):
     coor_azul=[]
     coor_naranja=[]
     atril=dar_letras(Letras,7)
+    A1=sg.Button(atril[0],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A1')
+    A2=sg.Button(atril[1],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A2')
+    A3=sg.Button(atril[2],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A3')
+    A4=sg.Button(atril[3],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A4')
+    A5=sg.Button(atril[4],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A5')
+    A6=sg.Button(atril[5],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A6')
+    A7=sg.Button(atril[6],button_color=('Black','#D8973C'),size=(6,3),pad=(0,0),border_width=(3),key='A7')
+    atril_k=['A1','A2','A3','A4','A5','A6','A7']
     puntaje_j=0
     puntaje_c=0
 
     top_10jugadores=leer_top()
 
     botones_c= lambda name: sg.Button(name,button_color=('#D8973C','black'),size=(6,3),pad=((0),0),border_width=(3))
-    button = lambda name : sg.Button(name,button_color=('Black','#D8973C'),size=(6,3),pad=((0),0),border_width=(3))
     button_j= lambda name : sg.Button(name,button_color=('#12947f','#12947f'),size=(3,1),pad=((0),0),border_width=(2),key=name)
 
     puntajes=[[sg.Text('PUNTAJE COMPUTADORA',text_color=('#D8C99B'),size=(19,1), font=("Courier New", 12))],
@@ -83,7 +99,8 @@ def main(nombre):
 
     layout = [[sg.Text('COMPUTADORA     ',text_color='#D8C99B',font=("Courier New", 20))]+[botones_c('?') for i in atril],
              [sg.Column(puntajes),sg.Frame(layout=[[button_j((col,fila))for col in range(15)] for fila in range(15)],border_width=(20),title='',pad=(0,7), relief=sg.RELIEF_SUNKEN,background_color=('#D8973C')),sg.Column(top10)],
-             [sg.Text('{}         '.format(nombre.upper()),text_color='#D8C99B',font=("Courier New", 20))]+[button(i) for i in atril]+[sg.Button('COMPROBAR',size=(15,1),button_color=('#A4243B','#D8973C'),border_width=(5),font=("Fixedsys",16))]]
+             [sg.Text('{}         '.format(nombre.upper()),text_color='#D8C99B',font=("Courier New", 20))]+[A1,A2,A3,A4,A5,A6,A7],
+             [sg.Button('COMPROBAR',size=(15,1),button_color=('#A4243B','#D8973C'),border_width=(5),font=("Fixedsys",16))]]
 
    
     
@@ -105,27 +122,45 @@ def main(nombre):
                 window.find_element((i,j)).update(button_color=('black','#ffb385'),text='P-5')#n
                 coor_naranja.append((i,j))
 
-    letras_elegidas=[]
+    palabra=''
     coordenadas=[]
-
+    key_letras=[]
+    
+    Check_button = lambda x: window.FindElement(x).Update(button_color=('#D8973C','black'))
+    Uncheck_button = lambda x: window.FindElement(x).Update(button_color=('Black','#D8973C'))
+    current_Check_button = ''
+    button_selected = False
+    current_button_selected = ''
+    
     while True:
         event, values = window.read()
         if event in (None, 'TERMINAR'):
             break
         if event in atril:
-            letras_elegidas.append(event)
-            coordenada=event
+            Check_button(event)
+            if button_selected:
+                if event == current_button_selected:
+                 Uncheck_button(event)
+                 button_selected = False
+                 current_button_selected = ''
+                else:
+                 Check_button(event)
+                 button_selected = True
+                 current_button_selected = event
+            letra_key=event
+            palabra+=window.find_element(letra_key).get_text()
+            key_letras.append(event)
             event,values=window.read()
             coordenadas.append(event)
-            window[event].update(coordenada, button_color=('Black','#D8973C'))
+            window[event].update(window.find_element(letra_key).get_text(), button_color=('Black','#D8973C'))
+            window[letra_key].update(button_color=('black','black'))
         if event == 'COMPROBAR':
-            print(letras_elegidas,coordenadas)
-            puntaje_j+=comprobar_puntaje(letras_elegidas,Letras)
+            print(palabra,coordenadas)
+            puntaje_j+=comprobar_puntaje(palabra,coordenadas,Letras,coor_rojos,coor_naranja,coor_azul,coor_celeste)
             window.find_element('puntaje_j').update(str(puntaje_j))
-            print(puntaje_j)
-            #cambiar_letras(window,atril,letras_elegidas,Letras)
-            print(atril)
-            letras_elegidas=[]
+            cambiar_letras(window,key_letras,Letras)
+            palabra=''
+            key_letras=[]
             coordenadas=[]
 
     window.close()
